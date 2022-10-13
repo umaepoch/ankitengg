b9f743e05400278275cd7fd9d3723839419ebe93
AkashMunikoti <akashmunikoti@gmail.com>
2022-10-13 12:27:04 +0530

    api for checking dublicate series

--- ankitengg/api.py
+++ ankitengg/api.py
@@ -5,6 +5,13 @@ from datetime import datetime
 
 @frappe.whitelist()
 def add_series_to_naming_series(pch_sc_item_series):
+    arr = ['-']
+    for i in arr:
+        if i in pch_sc_item_series:
+            print("True")
+        else:
+            pch_sc_item_series = pch_sc_item_series+'-'
+            print("False",pch_sc_item_series)
     doc=frappe.get_doc("Naming Series")
     print("doc.set_options",doc.set_options)
     series = doc.set_options
@@ -26,3 +33,17 @@ def add_series_to_naming_series(pch_sc_item_series):
         doc.save()
         frappe.db.commit()
     
+@frappe.whitelist()
+def allow_single_series(pch_sc_item_series):
+    print("....................")
+    print("name of first series ...........",pch_sc_item_series)
+    details_First=frappe.db.sql("""select pch_sc_item_series from `tabItem Group`""", as_dict=1)
+    print("parent first details",details_First)
+    value = pch_sc_item_series
+    list_of_all_values = [value for elem in details_First 
+    for value in elem.values()]
+    if value in list_of_all_values:
+        frappe.msgprint("This series is already Exist")
+        return value
+    else:
+        frappe.msgprint("This series already not Exist")
